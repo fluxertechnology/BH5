49689012aa230d1563008217367cdde5c5b9357b
zhengyanglee07 <zhengyangz1007@gmail.com>
2025-01-03 16:19:45 +0800

    fix: missing comic rank data

--- src/store/actions/pages/homeLeaderboardAction.js
+++ src/store/actions/pages/homeLeaderboardAction.js
@@ -10,7 +10,7 @@ export const getHomeLeaderBoardDataAction = (type) => {
     formData.append("page", 1);
     axiosRequest.post(postGetRankListData, formData).then(data=>{
       if (typeof data === "string") {
-        data = data.replace(/(1|0)RANK_MONTH10/g, "");
+        data = data.replace(/(1|0)RANK_MONTH[1-9][1-9]/g, "");
         data = JSON.parse(data).data;
       }
       
