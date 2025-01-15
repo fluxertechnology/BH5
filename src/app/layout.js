5556991aa75a343255b91849f00a334b41422851
Fluxer Technology <fluxertechnology.it@gmail.com>
2025-01-15 16:54:32 +0800

    refactor: change metadata to next.js method

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,36 +1,30 @@
-
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { GlobalProvider } from "@/store";
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google';
 import RootComponent from "@/components/common/RootComponent";
 
 import "@/styles/globals.scss";
 
+export async function generateMetadata() {
+  const messages = await getMessages();
+  const metadataTranslations = messages.Home;
+
+  return {
+    title: metadataTranslations.title,
+    description: metadataTranslations.description,
+  };
+}
 
 export default async function RootLayout({ children }) {
   const locale = await getLocale();
   const messages = await getMessages();
 
-  const metadataTranslations = messages.Home;
-
-  // Set metadata
-  const metadata = {
-    title: metadataTranslations.title,
-    description: metadataTranslations.description,
-  };
-
-
   return (
     <html lang={locale}>
-      <head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-      </head>
-
-      <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID} />
       <body>
+        <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID} />
         <NextIntlClientProvider locale={locale} messages={messages}>
           <GlobalProvider>
             <RootComponent locale={locale}>{children}</RootComponent>
