f5b82a58700891e28cddbf88a93295af6ec4ff66
Fluxer Technology <fluxertechnology.it@gmail.com>
2025-01-20 17:56:34 +0800

    fix: add dynamic domain on sitemap index

--- src/app/sitemap_index.xml/route.js
+++ src/app/sitemap_index.xml/route.js
@@ -4,60 +4,60 @@ import path from 'path';
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 const appPath = path.join(process.cwd(), 'src/app');
 
+export async function GET(req) {
+  const currentDomain = `${req.headers.get('x-forwarded-proto') || 'http'}://${req.headers.get('host')}`;
 
-export async function GET() {
-	const sitemaps = getSitemapFromDir(appPath);
+  const sitemaps = getSitemapFromDir(appPath);
 
-	const sitemapIndexXML = await buildSitemapIndex(sitemaps);
+  const sitemapIndexXML = await buildSitemapIndex(sitemaps, currentDomain);
 
-	return new Response(sitemapIndexXML, {
-		headers: {
-			'Content-Type': 'application/xml',
+  return new Response(sitemapIndexXML, {
+    headers: {
+      'Content-Type': 'application/xml',
       'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
       'Pragma': 'no-cache',
       'Expires': '0',
-		},
-	});
+    },
+  });
 }
 
-async function buildSitemapIndex(sitemaps) {
-	let xml = '<?xml version="1.0" encoding="UTF-8"?>';
-	xml += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+async function buildSitemapIndex(sitemaps, domain) {
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>';
+  xml += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
-	for (const sitemapURL of sitemaps) {
-		xml += '<sitemap>';
-		xml += `<loc>${siteUrl}${sitemapURL}</loc>`;
-		xml += '</sitemap>';
-	}
+  for (const sitemapURL of sitemaps) {
+    xml += '<sitemap>';
+    xml += `<loc>${domain}${sitemapURL}</loc>`;
+    xml += '</sitemap>';
+  }
 
-	xml += '</sitemapindex>';
-	return xml;
+  xml += '</sitemapindex>';
+  return xml;
 }
 
 const getSitemapFromDir = (dir, basePath = '') => {
-	const files = fs.readdirSync(dir);
-	let routes = [];
-	files.forEach((file) => {
-		const filePath = path.join(dir, file);
-		const stat = fs.statSync(filePath);
+  const files = fs.readdirSync(dir);
+  let routes = [];
+  files.forEach((file) => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
 
-		if (stat.isDirectory()) {
-			routes = routes.concat(
-				getSitemapFromDir(filePath, path.join(basePath, file)),
-			);
-		} else if (
-			(file.endsWith('sitemap.js'))
-		) {
-			const dirName = path.dirname(filePath);
-			const parentDir = dirName.replace(appPath, '');
-			routes.push(
-				`${parentDir}/${file.replace('.js', '')}.xml`,
-			);
-		}
-	});
+    if (stat.isDirectory()) {
+      routes = routes.concat(
+        getSitemapFromDir(filePath, path.join(basePath, file)),
+      );
+    } else if (
+      file.endsWith('sitemap.js')
+    ) {
+      const dirName = path.dirname(filePath);
+      const parentDir = dirName.replace(appPath, '');
+      routes.push(
+        `${parentDir}/${file.replace('.js', '')}.xml`,
+      );
+    }
+  });
 
-	return routes;
+  return routes;
 };
