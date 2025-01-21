b057ac423377d393fcb202a5bcffe6b008854ac5
Fluxer Technology <fluxertechnology.it@gmail.com>
2025-01-21 16:03:52 +0800

    fix: prevent back route on popup login dialog

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -13,90 +13,91 @@ const { home } = pageUrlConstants;
  * @return {*}
  */
 export const userLoginAction =
-  (params, callback = () => {}) =>
-  (dispatch) => {
-    console.log("users login action");
-    return utilitiesRequest
-      .getLoginAction(params)
-      .then((data) => {
-        if (data.id) {
-          window.localStorage.setItem(
-            "catchHash",
-            encryptionData(JSON.stringify(params))
-          );
+  (params, callback = () => { }) =>
+    (dispatch) => {
+      console.log("users login action");
+      return utilitiesRequest
+        .getLoginAction(params)
+        .then((data) => {
+          if (data.id) {
+            window.localStorage.setItem(
+              "catchHash",
+              encryptionData(JSON.stringify(params))
+            );
+            dispatch({
+              type: "INIT_USER",
+              data: {
+                ...data,
+                new_coupon_notification: data.new_coupon_notification,
+                is_video_card_opened: data.is_video_card_opened,
+              },
+            });
+            dispatch(updateRechargeStateAction(false));
+            updateUserDataAction((check) => {
+              callback(check);
+              if (check) {
+                setVipInfoAction(() => {
+                  if (window.location.pathname.startsWith("/login"))
+                    dispatch(replaceRoutes(home.pages.homeMain));
+                })(dispatch)
+              }
+            }, data.id)(dispatch);
+
+          } else {
+            callback(false);
+          }
+        })
+        .catch((err) => {
+          console.log("Error", "[/login/login/veify]:", err);
+        });
+    };
+
+export const userFBLoginAction =
+  (props, callback = () => { }) =>
+    async (dispatch) => {
+      const { id, accessToken } = props;
+      let response = await utilitiesRequest.postFBLoginAction(accessToken);
+      if (!response.code) return;
+      let params = { username: id, passwd: id };
+      await dispatch(userLoginAction(params, callback));
+    };
+
+export const updateUserDataAction =
+  (callback = () => { }, id = null) =>
+    (dispatch) => {
+      const userId = id ? id : store.getState().user.id;
+      return utilitiesRequest
+        .postUpdateUserAction(userId)
+        .then((infoData) => {
           dispatch({
             type: "INIT_USER",
             data: {
-              ...data,
-              new_coupon_notification: data.new_coupon_notification,
-              is_video_card_opened: data.is_video_card_opened,
+              ...infoData,
+              id: userId,
+              avatar: infoData.avatar,
+              username: infoData.username,
+              nick_name: infoData.nick_name,
+              sex: infoData.sex,
+              time: infoData.time,
+              rank: infoData.rank,
+              day_usedviewcount: infoData.day_usedviewcount,
+              day_maxviewcount: infoData.day_maxviewcount,
+              day_share: infoData.day_share,
+              sign: infoData.sign,
+              money: infoData.money,
+              share_ma: infoData.share_ma,
+              free_gashapon: infoData.free_gashapon,
+              // ...infoData,
+              // ...data,
             },
           });
-          dispatch(updateRechargeStateAction(false));
-          updateUserDataAction((check) => {
-            callback(check);
-            if (check) {
-              setVipInfoAction(() => {
-                dispatch(replaceRoutes(home.pages.homeMain));
-              })(dispatch)
-            }
-          }, data.id)(dispatch);
-  
-        } else {
+          callback(true);
+        })
+        .catch((err) => {
+          console.log(err);
           callback(false);
-        }
-      })
-      .catch((err) => {
-        console.log("Error", "[/login/login/veify]:", err);
-      });
-  };
-
-export const userFBLoginAction =
-  (props, callback = () => {}) =>
-  async (dispatch) => {
-    const { id, accessToken } = props;
-    let response = await utilitiesRequest.postFBLoginAction(accessToken);
-    if (!response.code) return;
-    let params = { username: id, passwd: id };
-    await dispatch(userLoginAction(params, callback));
-  };
-
-export const updateUserDataAction =
-  (callback = () => {}, id = null) =>
-  (dispatch) => {
-    const userId = id ? id : store.getState().user.id;
-    return utilitiesRequest
-      .postUpdateUserAction(userId)
-      .then((infoData) => {
-        dispatch({
-          type: "INIT_USER",
-          data: {
-            ...infoData,
-            id: userId,
-            avatar: infoData.avatar,
-            username: infoData.username,
-            nick_name: infoData.nick_name,
-            sex: infoData.sex,
-            time: infoData.time,
-            rank: infoData.rank,
-            day_usedviewcount: infoData.day_usedviewcount,
-            day_maxviewcount: infoData.day_maxviewcount,
-            day_share: infoData.day_share,
-            sign: infoData.sign,
-            money: infoData.money,
-            share_ma: infoData.share_ma,
-            free_gashapon: infoData.free_gashapon,
-            // ...infoData,
-            // ...data,
-          },
         });
-        callback(true);
-      })
-      .catch((err) => {
-        console.log(err);
-        callback(false);
-      });
-  };
+    };
 
 export const userLoginOutAction = () => {
   return {
@@ -125,17 +126,17 @@ export const userFBLoginOutAction = () => {
 
 export const openPopup =
   (popupType) =>
-  (dispatch) => {
-    document.getElementById("popup-dialog").style.display = "block";
-    document.getElementsByTagName("body")[0].style.overflow = "hidden";
+    (dispatch) => {
+      document.getElementById("popup-dialog").style.display = "block";
+      document.getElementsByTagName("body")[0].style.overflow = "hidden";
 
-    dispatch({
-      type: "UPDATE_POPUP_TYPE",
-      data: {
-        popupType,
-      },
-    });
-  };
+      dispatch({
+        type: "UPDATE_POPUP_TYPE",
+        data: {
+          popupType,
+        },
+      });
+    };
 
 export const closePopup = () => {
   return function () {
