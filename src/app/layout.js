4ddd7bdd80b5e3c588e522f61647e529cc32dcee
Fluxer Technology <fluxertechnology.it@gmail.com>
2025-01-23 10:50:21 +0800

    refactor: update previous matomo script on layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,15 +29,24 @@ export default async function RootLayout({ children }) {
       <body>
         <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID} />
 
-        {/* Matomo Tag Manager */}
-        <Script id="matomo" strategy="afterInteractive">
+
+        {/* Matomo Tracking Script */}
+        <Script id="matomo-tracking" strategy="afterInteractive">
           {`
-            var _mtm = window._mtm = window._mtm || [];
-            _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-            (function() {
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true; g.src='https://beastdatab.com/js/container_s2X8bY7d.js'; 
-              s.parentNode.insertBefore(g,s);
+            var _paq = (window._paq = window._paq || []);
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(["trackPageView"]);
+            _paq.push(["enableLinkTracking"]);
+            (function () {
+              var u = "https://mtm.shenwujs.cn/";
+              _paq.push(["setTrackerUrl", u + "matomo.php"]);
+              _paq.push(["setSiteId", "1"]);
+              var d = document,
+                g = d.createElement("script"),
+                s = d.getElementsByTagName("script")[0];
+              g.async = true;
+              g.src = u + "matomo.js";
+              s.parentNode.insertBefore(g, s);
             })();
           `}
         </Script>
