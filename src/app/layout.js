a261c2d082645c66845008c8aeaaebd20fb19344
Fluxer Technology <fluxertechnology.it@gmail.com>
2025-01-23 10:55:20 +0800

    refactor: update matomo script on layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,20 +33,16 @@ export default async function RootLayout({ children }) {
         {/* Matomo Tracking Script */}
         <Script id="matomo-tracking" strategy="afterInteractive">
           {`
-            var _paq = (window._paq = window._paq || []);
+            var _paq = window._paq = window._paq || [];
             /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-            _paq.push(["trackPageView"]);
-            _paq.push(["enableLinkTracking"]);
-            (function () {
-              var u = "https://mtm.shenwujs.cn/";
-              _paq.push(["setTrackerUrl", u + "matomo.php"]);
-              _paq.push(["setSiteId", "1"]);
-              var d = document,
-                g = d.createElement("script"),
-                s = d.getElementsByTagName("script")[0];
-              g.async = true;
-              g.src = u + "matomo.js";
-              s.parentNode.insertBefore(g, s);
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="https://beastdatab.com/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
             })();
           `}
         </Script>
