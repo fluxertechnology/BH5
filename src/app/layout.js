27e6aaad63af67f577bf786588701fcd9eadae32
Fluxer Technology <fluxertechnology.it@gmail.com>
2025-01-23 10:41:13 +0800

    refactor: update matomo script on layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,58 +9,56 @@ import Script from 'next/script';
 import '@/styles/globals.scss';
 
 export async function generateMetadata() {
-	const messages = await getMessages();
-	const metadataTranslations = messages.Home;
+  const messages = await getMessages();
+  const metadataTranslations = messages.Home;
 
-	return {
-		title: metadataTranslations.title,
-		description: metadataTranslations.description,
-		keywords:
-			'成人动漫, 里番, 本子, 工口, 绅士向, 成人漫画, H本, 成人动画, H游戏, 福利姬',
-	};
+  return {
+    title: metadataTranslations.title,
+    description: metadataTranslations.description,
+    keywords:
+      '成人动漫, 里番, 本子, 工口, 绅士向, 成人漫画, H本, 成人动画, H游戏, 福利姬',
+  };
 }
 
 export default async function RootLayout({ children }) {
-	const locale = await getLocale();
-	const messages = await getMessages();
+  const locale = await getLocale();
+  const messages = await getMessages();
 
-	return (
-		<html lang={locale}>
-			<body>
-				<GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID} />
+  return (
+    <html lang={locale}>
+      <body>
+        <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID} />
 
-				<Script
-					id='matomo-tracker'
-					strategy='afterInteractive'
-					dangerouslySetInnerHTML={{
-						__html: `
-              var _mtm = window._mtm = window._mtm || [];
-              _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-              (function() {
-                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                g.async=true; g.src='https://beastdatab.com/js/container_s2X8bY7d.js'; s.parentNode.insertBefore(g,s);
-              })();
-          `,
-					}}
-				/>
+        {/* Matomo Tag Manager */}
+        <Script id="matomo" strategy="afterInteractive">
+          {`
+            var _mtm = window._mtm = window._mtm || [];
+            _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+            (function() {
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src='https://beastdatab.com/js/container_s2X8bY7d.js'; 
+              s.parentNode.insertBefore(g,s);
+            })();
+          `}
+        </Script>
 
-				{/* Google Tag Manager NoScript */}
-				<noscript>
-					<iframe
-						src='https://www.googletagmanager.com/ns.html?id=GTM-WN8PS24'
-						height='0'
-						width='0'
-						style={{ display: 'none', visibility: 'hidden' }}
-					>
-					</iframe>
-				</noscript>
+        {/* Google Tag Manager NoScript */}
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-WN8PS24'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          >
+          </iframe>
+        </noscript>
 
-				<NextIntlClientProvider locale={locale} messages={messages}>
-					<GlobalProvider>
-						<RootComponent locale={locale}>{children}</RootComponent>
-					</GlobalProvider>
-				</NextIntlClientProvider>
-			</body>
-		</html>
-	);
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <GlobalProvider>
+            <RootComponent locale={locale}>{children}</RootComponent>
+          </GlobalProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
