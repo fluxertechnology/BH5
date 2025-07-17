7ffe2f418b356fd7baf4821d553a8eec8f242e89
zhengyanglee07 <zhengyangz1007@gmail.com>
2025-07-17 23:45:21 +0800

    fix: end game failed

--- src/app/api/tcg/game-session/route.ts
+++ src/app/api/tcg/game-session/route.ts
@@ -2,9 +2,10 @@ const activeGames = new Map();
 
 export async function POST(req) {
   const body = await req.json();
-  const { userId, gameId, action } = body;
+  const { userId: crrUserId, gameId, action } = body;
 
-  // console.log(activeGames);
+  const userId = crrUserId.toString().trim().toLowerCase();
+
   console.log("Received request:", body);
   console.log("Active games:", activeGames);
 
