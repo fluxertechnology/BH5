72860129a9b784ad047c746c0611c06a6a63fdd6
jd3270 <jd-crm3270@hotmail.com>
2025-08-06 22:54:13 +0800

    feat: category page cant fetch comic type

--- src/app/home/main/category/[category]/page.js
+++ src/app/home/main/category/[category]/page.js
@@ -4,7 +4,12 @@ import React, { useState, useEffect, useRef, use, useCallback } from "react";
 import { useTranslations } from "next-intl";
 import Grid from "@mui/material/Grid";
 import styled from "styled-components";
-import { adsKeys, colors, side_padding } from "@/lib/constants";
+import {
+  adsKeys,
+  colors,
+  side_padding,
+  pageUrlConstants,
+} from "@/lib/constants";
 import WavaButton from "@/components/layout/Header/WavaButton";
 import scrollBottomCallEvent from "@/lib/services/scrollEvent";
 import { useGlobalContext, useGlobalDispatch } from "@/store";
@@ -13,6 +18,7 @@ import useMediaQuery from "@/hooks/useMediaQuery";
 import TabLabel from "@/components/index/TabLabel";
 import CoverCubeItem from "@/components/common/CoverCubeItem";
 import { usePathname, useParams } from "next/navigation";
+import { pushRoutes } from "@/store/actions/historyActions";
 
 import {
   getCategoryDataAction,
@@ -26,14 +32,24 @@ const HomeCategoryPage = () => {
   const { state } = useGlobalContext();
   const t = useTranslations();
   const loaction = usePathname();
+  const { home } = pageUrlConstants;
 
   const { isMobile } = useMediaQuery();
 
   const titleLocale = {
-    'K-Comics': "韩漫",
-    'Dojin': "同人",
-    'Free Watch': "Free",
-  }
+    "K-Comics": "韩漫",
+    Dojin: "同人",
+    "Free Watch": "Free",
+  };
+  const typeMapping = {
+    [t("Global.animate")]: 0,
+    [t("Global.3d")]: 0,
+    [t("Global.j_comics")]: 1,
+    [t("Global.k_comics")]: 2,
+    [t("Global.e_comics")]: 3,
+    [t("Navbar.top_navigator_novel")]: 4,
+    [t("Global.visual_text")]: 5,
+  };
   const decodeTitle = decodeURIComponent(useParams().category);
   const title = titleLocale[decodeTitle] ?? decodeTitle;
 
@@ -43,9 +59,7 @@ const HomeCategoryPage = () => {
 
   const [tabHeightState, setTabHeightState] = useState(false);
 
-  const [type, setType] = useState(
-    title === t("Global.animate") || title === t("Global.3d") ? 0 : 1
-  );
+  const [type, setType] = useState(typeMapping[title] ?? 1);
   const [pickCategory, setPickCategory] = useState(
     state.homeCategoryData[title]?.select_tag_gp || []
   );
@@ -71,8 +85,7 @@ const HomeCategoryPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-
-  const [ isInit, setIsInit ] = useState(true);
+  const [isInit, setIsInit] = useState(true);
   useEffect(() => {
     getListData(true);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -115,18 +128,17 @@ const HomeCategoryPage = () => {
       window.removeEventListener("resize", updateIs750);
     };
   }, []);
-  
+
   useEffect(() => {
     getTabHeight();
   }, [is750, isMobile]);
 
-
   const toggleTabHeight = useCallback(() => {
     if (!tabRef) return;
     if (tabRef.current.offsetHeight === tabHeight) {
-      if(isMobile){
+      if (isMobile) {
         tabRef.current.style.height = is750 ? "100px" : "72px";
-      }else{
+      } else {
         tabRef.current.style.height = "100px";
       }
       setTabHeightState(false);
@@ -141,9 +153,9 @@ const HomeCategoryPage = () => {
 
     tabRef.current.style.height = "unset";
     setTabHeight(tabRef.current.offsetHeight);
-    if(isMobile){
+    if (isMobile) {
       tabRef.current.style.height = is750 ? "100px" : "72px";
-    }else{
+    } else {
       tabRef.current.style.height = "100px";
     }
   }, [is750, isMobile, tabHeight, tabRef]);
@@ -168,13 +180,17 @@ const HomeCategoryPage = () => {
   // }
 
   function getListData(init = false) {
-    getCategoryData({
-      init,
-      type: type,
-      category: title,
-      is_free: pickPrice,
-      tag_gp: pickCategory,
-    }, () => { if (init) setIsInit(false) }
+    getCategoryData(
+      {
+        init,
+        type: type,
+        category: title,
+        is_free: pickPrice,
+        tag_gp: pickCategory,
+      },
+      () => {
+        if (init) setIsInit(false);
+      }
     );
   }
   function onSelectCategory(name) {
@@ -195,6 +211,34 @@ const HomeCategoryPage = () => {
   const resetSetCategoryData = (category) => {
     useGlobalDispatch(restCategoryDataAction(category));
   };
+
+  const handleTabChange = (newType, categoryTitle) => {
+    if (type === newType && title === categoryTitle) return;
+
+    setType(newType);
+
+    // 清空旧数据
+    useGlobalDispatch(restCategoryDataAction(categoryTitle));
+
+    // 更新路由，这样 title 也会自动变（useParams）
+    useGlobalDispatch(
+      pushRoutes({
+        name: home.pages.homeMain.pages.homeCategory.name + categoryTitle,
+        path: home.pages.homeMain.pages.homeCategory.path,
+        dynamic: {
+          tab: categoryTitle,
+        },
+      })
+    );
+
+    // 不需要 setTitle()
+  };
+
+  useEffect(() => {
+    setPickCategory(state.homeCategoryData[title]?.select_tag_gp || []);
+    setPickPrice(title === t("Global.free_for_a_limited_time") ? 2 : 0);
+  }, [title]);
+
   return (
     <HomeCategoryElement className={!isMobile && "px-indent"}>
       <ImageCarousel
@@ -211,33 +255,18 @@ const HomeCategoryPage = () => {
           <div className="category_container_content_box">
             <TabLabel
               text={t("Global.j_comics")}
-              active={type === 1}
-              onClick={() => {
-                if (type !== 1) {
-                  resetSetCategoryData(title);
-                  setType(1);
-                }
-              }}
+              active={title === t("Global.j_comics")}
+              onClick={() => handleTabChange(1, t("Global.j_comics"))}
             />
             <TabLabel
               text={t("Global.k_comics")}
-              active={type === 2}
-              onClick={() => {
-                if (type !== 2) {
-                  resetSetCategoryData(title);
-                  setType(2);
-                }
-              }}
+              active={title === t("Global.k_comics")}
+              onClick={() => handleTabChange(2, t("Global.k_comics"))}
             />
             <TabLabel
               text={t("Global.e_comics")}
-              active={type === 3}
-              onClick={() => {
-                if (type !== 3) {
-                  resetSetCategoryData(title);
-                  setType(3);
-                }
-              }}
+              active={title === t("Global.e_comics")}
+              onClick={() => handleTabChange(3, t("Global.e_comics"))}
             />
             {"韩漫".indexOf(title) === -1 && (
               <TabLabel
@@ -304,7 +333,9 @@ const HomeCategoryPage = () => {
             onClick={toggleTabHeight}
           >
             <WavaButton className="category_container_content_btn_button">
-              &lt;&nbsp;&nbsp;&nbsp;{tabHeightState ? t("Global.show_less") : t("Global.show_more")}&nbsp;&nbsp;&nbsp;&gt;
+              &lt;&nbsp;&nbsp;&nbsp;
+              {tabHeightState ? t("Global.show_less") : t("Global.show_more")}
+              &nbsp;&nbsp;&nbsp;&gt;
             </WavaButton>
           </div>
         </div>
@@ -361,7 +392,9 @@ const HomeCategoryPage = () => {
                   md={type === 0 ? 2.4 : 1.71}
                   xs={type === 0 ? 6 : 4}
                   key={`${data.id}-${index}`}
-                  className={`${(type === 4 || type === 5) ? 'novel-illust-adj': ''}`}
+                  className={`${
+                    type === 4 || type === 5 ? "novel-illust-adj" : ""
+                  }`}
                 >
                   <CoverCubeItem
                     isVideo={type === 0}
@@ -387,7 +420,7 @@ export const HomeCategoryElement = styled.div`
   .category_container {
     padding: 48px ${side_padding}px 10px;
 
-    @media (max-width: 898px){
+    @media (max-width: 898px) {
       padding: 6.67vw ${side_padding}px 10px;
     }
 
@@ -409,7 +442,7 @@ export const HomeCategoryElement = styled.div`
           border-top: 1px solid ${colors.back_grey};
           border-radius: 5px;
 
-          @media (max-width: 898px){
+          @media (max-width: 898px) {
             font-weight: 400;
             font-size: max(12px, 2.4vw);
           }
@@ -423,7 +456,7 @@ export const HomeCategoryElement = styled.div`
       padding-top: 1%;
     }
 
-    .novel-illust-adj .item_body div{
+    .novel-illust-adj .item_body div {
       padding-bottom: 101.55%;
     }
   }
